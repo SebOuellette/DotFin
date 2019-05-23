718f144504becbfb6c34e087ea67e9b7a8f55d6e
PlayBy <bigbrosebas@gmail.com>
2019-05-23 19:01:44 -0400

    Worked more on the lexer

--- lexer/lexer.hpp
+++ lexer/lexer.hpp
@@ -18,16 +18,22 @@ std::vector<Token> lex(std::string file) {
         
         // List of regexes in same order as if statements that handle them below
         std::regex functionCheck("^ *(\\w+)?(?=\\()");
-        std::regex conditionBracketCheck("^ *\\(");
-        std::regex variableCheck("^ *var");
+        std::regex bracketCheck("^ *\\(|\\)|\\{|\\}|\\[|\\]");
+        std::regex variableCheck("^ *var *(\\w+)");
+        std::regex stringCheck("^ *\"(\w+)\"");
+        std::regex varReferenceCheck("^ *(\w+)");
 
         // List of if statements to lex the next segment into a token
         if (std::regex_search(toParse, match, functionCheck)) {
             tokenList.push_back(handleLex(&toParse, match[1], functionCheck, "function"));
-        } else if (std::regex_search(toParse, match, conditionBracketCheck)) {
-            tokenList.push_back(handleLex(&toParse, match[1], conditionBracketCheck, "conditionBracketOpen"));
+        } else if (std::regex_search(toParse, match, bracketCheck)) {
+            tokenList.push_back(handleLex(&toParse, match[1], bracketCheck, "bracket"));
         } else if (std::regex_search(toParse, match, variableCheck)) {
-            tokenList.push_back(handleLex(&toParse, match[1], variableCheck, "newVariable"));
+            tokenList.push_back(handleLex(&toParse, match[1], variableCheck, "newVar"));
+        } else if (std::regex_search(toParse, match, stringCheck)) {
+            tokenList.push_back(handleLex(&toParse, match[1], stringCheck, "string"));
+        } else if (std::regex_search(toParse, match, varReferenceCheck)) {
+            tokenList.push_back(handleLex(&toParse, match[1], varReferenceCheck, "varRef"));
         } else {
             found = false;
         }
